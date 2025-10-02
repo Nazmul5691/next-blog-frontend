fe65165e6c7398a24facf59b21e880012b6f927d
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-10-02 12:09:11 +0600

    first commit

---
+++ src/app/(public)/blogs/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from "@/components/ui/Loading";
+
+export default function BlogLoadingPage() {
+    return (
+        <div className="py-20">
+            <Loading />
+        </div>
+    );
+}
